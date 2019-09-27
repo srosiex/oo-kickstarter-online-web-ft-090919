03a6ce6162d90107d2576ae161a520d5a9a2a2f4
Sarah H <sarahrose.henderson@gmail.com>
2019-09-27 14:49:56 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,10 +8,19 @@ class Backer
   end
 
 def back_project(project)
-  @backed_projects << Project(project).new
-  
+  # @backed_projects << Project(project).new
+
 end
 
 end
 
 #backer_instance . back_project
+# describe 'Project - #add_backer' do
+#   it 'accepts a Backer as an argument and stores it in a backers array' do
+#     book = Project.new("Ruby, Ruby, and More Ruby")
+#     steven = Backer.new("Steven")
+#
+#     # Same here. We are actually passing around a Backer object. This is pretty
+#     # simple functionality, but objects can interact with one another in really
+#     # cool ways.
+#     book.add_backer(steven)
