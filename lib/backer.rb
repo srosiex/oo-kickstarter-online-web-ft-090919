b94c7256b7ae06e4c92e7695fd743ecb9429991c
Sarah H <sarahrose.henderson@gmail.com>
2019-09-27 15:23:54 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,28 @@
+class Backer
+  attr_reader :name
+  attr_accessor :backed_projects
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    @backed_projects << project
+      project.backers << self
+    end
+
+
+
+#backer_instance . back_project
+# describe 'Backer - #back_project' do
+#   it 'accepts a Project as an argument and stores it in a backed_projects array' do
+#     spencer = Backer.new("Spencer")
+#     magic = Project.new("Magic The Gathering Thing")
+
+    # If we are calling this method in this way, what type of argument is it taking?
+    # # We are actually passing in a Project object! Cool, huh?
+    # spencer.back_project(magic)
+    # expect(spencer.backed_projects).to include(magic)
+
+end
---
+++ lib/project.rb
@@ -0,0 +1,22 @@
+class Project
+
+attr_reader :title
+attr_accessor :backers
+
+def initialize(title)
+  @title = title
+  @backers = []
+
+end
+
+def project
+  @@all
+end
+
+def add_backer(backer)
+  @backers << backer
+  backer.backed_projects << self
+
+end
+
+end
