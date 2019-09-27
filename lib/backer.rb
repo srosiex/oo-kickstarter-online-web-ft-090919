507fe3173a163e762efc8f041a7b51851d9eb011
Sarah H <sarahrose.henderson@gmail.com>
2019-09-27 14:34:29 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,7 +2,7 @@ class Backer
   attr_reader :name
   attr_accessor :backed_projects
 
-  def initialize(name, backed_projects)
+  def initialize(name)
     @name = name
     @backed_projects = []
   end
