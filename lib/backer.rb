58c256d9ff59645c476a05b7e4904c8fbf76f76d
Sarah H <sarahrose.henderson@gmail.com>
2019-09-27 14:52:50 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -16,10 +16,10 @@ class Backer
 end
 
 #backer_instance . back_project
-describe 'Backer - #back_project' do
-  it 'accepts a Project as an argument and stores it in a backed_projects array' do
-    spencer = Backer.new("Spencer")
-    magic = Project.new("Magic The Gathering Thing")
+# describe 'Backer - #back_project' do
+#   it 'accepts a Project as an argument and stores it in a backed_projects array' do
+#     spencer = Backer.new("Spencer")
+#     magic = Project.new("Magic The Gathering Thing")
 
     # If we are calling this method in this way, what type of argument is it taking?
     # We are actually passing in a Project object! Cool, huh?
